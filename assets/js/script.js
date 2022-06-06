435f8382a28627bdf116c3517b1d6ccd82602775
Montez Burt <Griffenc4@gmail.com>
2022-06-05 21:57:24 -0500

    page will now remeber info even after refreshing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,40 @@
 
         localStorage.setItem(time, text);
      })
-     
- })
+
+     function timeTracker() {
+         var timeNow = moment().hour();
+
+         $(".time-block").each(function() {
+             var blockTime = parseInt($(this).attr("id").split("hour")[1])
+            // check the time and add classes for indicators in the background
+             if (blockTime < timeNow) {
+                 $(this).removeClass("future");
+                 $(this).removeClass("present");
+                 $(this).addClass("present");
+             } else if (blockTime === timeNow) {
+                 $(this).removeClass("past");
+                 $(this).removeClass("present");
+                 $(this).addClass("present");
+             } else {
+                 $(this).removeClass("present")
+                 $(this).removeClass("past");
+                 $(this).addClass("future");
+             }
+         })
+     }
+     // grabbing entered item from local storage
+ 
+     $("hour8 .description").val(localStorage.getItem("hour8"));
+     $("#hour9 .description").val(localStorage.getItem("hour9"));
+     $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+ 
+     timeTracker();
+})
